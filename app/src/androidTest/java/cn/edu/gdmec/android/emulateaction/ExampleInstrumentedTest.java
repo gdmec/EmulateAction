662ec166b844a2de3cf2a7222a0c7dc015b0f552
cuiyu <cuiyu@gdmec.edu.cn>
2017-10-26 10:28:35 +0800

    取消全部的管理员权限.

--- app/src/androidTest/java/cn/edu/gdmec/android/emulateaction/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cn/edu/gdmec/android/emulateaction/ExampleInstrumentedTest.java
@@ -14,6 +14,7 @@ import android.support.test.uiautomator.UiObjectNotFoundException;
 import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
 import android.support.test.uiautomator.Until;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -70,8 +71,46 @@ public class ExampleInstrumentedTest {
         mDevice.swipe(200, 600, 200, 200, 50);
         sleep(100);
         mDevice.findObject(new UiSelector().textStartsWith("设备管理器")).clickAndWaitForNewWindow();
-        mDevice.findObject(new UiSelector().textStartsWith("MobileGuard")).clickAndWaitForNewWindow();
-        mDevice.findObject(new UiSelector().textStartsWith("取消激活")).clickAndWaitForNewWindow();
+        UiScrollable list = new UiScrollable( new UiSelector().className("android.widget.ListView"));
+        int count = list.getChildCount();
+        for(int i=0;i<count;i++){
+            UiObject apps = null;
+            try {
+                //这个listview会动态调整顺序，每次更改后把选中的排在前面，并且index好像是双倍的。
+                //取消Sample Device Admin是会多弹出一个对话框，很坑。
+                apps = list.getChildByInstance(new UiSelector().className("android.widget.LinearLayout"), 0);
+//                try{
+//                    result = apps.getChild(new UiSelector().textStartsWith("Dummy"));
+//                    if(result.exists()){
+//                        continue;
+//                    }
+//                    result = apps.getChild(new UiSelector().textStartsWith("Sample"));
+//                    if(result.exists()){
+//                        continue;
+//                    }
+//                }catch (UiObjectNotFoundException e){}
+
+                apps.clickAndWaitForNewWindow();
+                mDevice.swipe(200, 700, 200, 100, 100);
+                sleep(100);
+                try{
+                    result = mDevice.findObject(new UiSelector().textStartsWith("取消激活"));
+                    if(result==null||!result.exists()){
+                        result = mDevice.findObject(new UiSelector().textStartsWith("取消"));
+                        result.clickAndWaitForNewWindow();
+                    }else{
+                        result.clickAndWaitForNewWindow();
+                        //取消Sample Device Admin是会多弹出一个对话框，很坑。
+                        result = mDevice.findObject(new UiSelector().textStartsWith("确定"));
+                        if(result.exists()){
+                            result.clickAndWaitForNewWindow();
+                        }else{
+                            continue;
+                        }
+                    }
+                }catch (UiObjectNotFoundException e){}
+            } catch(UiObjectNotFoundException e) {}
+        }
     }
 
 }
